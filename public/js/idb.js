b812454b54e35f4d5ff0c0d299c0321a6dc1ee24
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-16 01:26:02 -0400

    Update idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -12,3 +12,14 @@ request.onupgradeneeded = function (event) {
   // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
   db.createObjectStore("new_transaction", { autoIncrement: true });
 };
+
+// upon a successful
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store or simply established a connection, save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+  if (navigator.onLine) {
+    // todo: uploadTransaction();
+  }
+};
